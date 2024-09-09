7dae9688816a6f35c441cde0b40f08bd3a5e6b22
kelvin <kelvin.oy16@gmail.com>
2024-09-09 22:14:37 +0000

    Added new page

--- unreal/src/Components/transferSteps.jsx
+++ unreal/src/Components/transferSteps.jsx
@@ -7,16 +7,16 @@ export default function TransferSteps(){
     return(
         <div className="transferContainer">
             <div className="page">
-            <div className="send_steps">
-                 <h1>How to transfer money?</h1>
-                 <hr/>
-                 <ol type="1">
-                     <li><b>Create an account</b> This is easy. Sign up using your email address on our website. Keep things secure by using a strong password. </li>
-                     <li><b>Start your transfer</b> When signed in, you would see a side panel on the left side of your screen. Click on send money.</li>
-                     <li><b>Enter the recipient's details</b> FIll in all the fields provided. Make sure to enter the correct details required.</li>
-                     <li><b>Verify the details</b> Make sure you verify the details before completing the transfer.</li>
-                 </ol>
-            </div>
+                <div className="send_steps">
+                     <h1>How to transfer money?</h1>
+                     <hr/>
+                     <ol type="1">
+                         <li><b>Create an account</b> This is easy. Sign up using your email address on our website. Keep things secure by using a strong password. </li>
+                         <li><b>Start your transfer</b> When signed in, you would see a side panel on the left side of your screen. Click on send money.</li>
+                         <li><b>Enter the recipient's details</b> FIll in all the fields provided. Make sure to enter the correct details required.</li>
+                         <li><b>Verify the details</b> Make sure you verify the details before completing the transfer.</li>
+                     </ol>
+                </div>
             <div className="steps_img">
                 <img src={womanImage} alt=""/>
             </div>
